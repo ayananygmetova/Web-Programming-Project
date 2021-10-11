d2a4fd6949b932b82da7e8e287e02383f3fb2531
damirbaisalov <baisalovdamir@gmail.com>
2021-10-12 00:29:43 +0600

    optimized function for openTab

--- pages/user-channel/script.js
+++ pages/user-channel/script.js
@@ -1,14 +1,9 @@
-function openTab(evt, tabName, horizontalLine) {
-    var i, tabcontent, tablinks;
+function openTab(tabName, horizontalLine) {
+    var i, tabcontent;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
     document.getElementById(tabName).style.display = "block";
     document.getElementById(horizontalLine).style.display = "block";
-    evt.currentTarget.className += " active";
   }
